246cd5baef58be6154912a69e9050bb73e16d3ca
Zekican Aydin <zeki-can-16@hotmail.com>
2023-12-01 21:52:03 +0300

    patch: created splash screen

--- src/navigations/MainStackNavigator.tsx
+++ src/navigations/MainStackNavigator.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import CustomTopBar from '../components/CustomTopBar';
+import SplashScreen from '../screens/SplashScreen';
 
 type MainStackParams = {
+  SplashScreen: undefined;
   HomeScreen: undefined;
 };
 
 const MainNavigationWrapper: React.FC = () => {
   const MainStack = createStackNavigator<MainStackParams>();
   return (
-    <MainStack.Navigator initialRouteName="HomeScreen">
+    <MainStack.Navigator initialRouteName="SplashScreen">
       <MainStack.Screen
         options={() => ({
           title: 'E-Market',
@@ -24,6 +26,13 @@ const MainNavigationWrapper: React.FC = () => {
         })}
         name="HomeScreen"
         component={HomeScreen}
+      />
+      <MainStack.Screen
+        options={() => ({
+          headerShown: false,
+        })}
+        name="SplashScreen"
+        component={SplashScreen}
       />
     </MainStack.Navigator>
   );
---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,25 @@
+import React, {useEffect} from 'react';
+import {useAppDispatch} from '../redux/hooks';
+import {setGlobalLoadState} from '../redux/slices/globalLoadState';
+import {getProducts} from '../redux/slices/productsSlice';
+
+const SplashScreen: React.FC = ({navigation}) => {
+  const dispatch = useAppDispatch();
+
+  const goHomeScreen = async () => {
+    dispatch(setGlobalLoadState(true));
+    await dispatch(getProducts());
+    setTimeout(() => {
+      navigation.navigate('HomeScreen');
+    }, 500);
+    return dispatch(setGlobalLoadState(false));
+  };
+
+  useEffect(() => {
+    goHomeScreen();
+  }, []);
+
+  return <></>;
+};
+
+export default SplashScreen;
