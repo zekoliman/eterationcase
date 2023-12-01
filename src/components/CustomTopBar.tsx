400f015ac197bcbbae8f949d1d1452bf4c28beae
Zekican Aydin <zeki-can-16@hotmail.com>
2023-12-01 20:37:49 +0300

    patch: custom top bar component created and stack navigator changed

---
+++ src/components/CustomTopBar.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  StackNavigationOptions,
+  StackNavigationProp,
+} from '@react-navigation/stack';
+import {ParamListBase, RouteProp} from '@react-navigation/native';
+import {
+  BottomTabNavigationOptions,
+  BottomTabNavigationProp,
+} from '@react-navigation/bottom-tabs';
+import {getHeaderTitle} from '@react-navigation/elements';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Colors from '../theme/Colors';
+import NavigationLeftSvg from '../assets/icons/LeftArrowSvg';
+
+type NavigationBarTypes = {
+  navigation:
+    | StackNavigationProp<ParamListBase>
+    | BottomTabNavigationProp<ParamListBase>;
+  route: RouteProp<ParamListBase>;
+  options: StackNavigationOptions | BottomTabNavigationOptions;
+};
+
+const CustomTopBar = ({navigation, route, options}: NavigationBarTypes) => {
+  const screenTitle = getHeaderTitle(options, route?.name);
+
+  const goBack = () => {
+    navigation.goBack();
+  };
+
+  const renderHeader = () => {
+    return (
+      <Text
+        style={[
+          styles.headerTitle,
+          {marginHorizontal: navigation.canGoBack() ? 77 : 16},
+        ]}>
+        {screenTitle}
+      </Text>
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerView}>
+        {navigation.canGoBack() && (
+          <TouchableOpacity
+            style={styles.leftIconView}
+            activeOpacity={0.75}
+            onPress={goBack}>
+            <NavigationLeftSvg />
+          </TouchableOpacity>
+        )}
+        {renderHeader()}
+      </View>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.mainColor,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  headerView: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    fontWeight: '800',
+    fontSize: 24,
+    color: Colors.white,
+  },
+
+  leftIconView: {
+    position: 'absolute',
+    left: 0,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+});
+export default CustomTopBar;
--- src/navigations/MainStackNavigator.tsx
+++ src/navigations/MainStackNavigator.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import CustomTopBar from '../components/CustomTopBar';
 
 type MainStackParams = {
   HomeScreen: undefined;
 };
 
 const MainNavigationWrapper: React.FC = () => {
-  const MainStack = createNativeStackNavigator<MainStackParams>();
+  const MainStack = createStackNavigator<MainStackParams>();
   return (
     <MainStack.Navigator initialRouteName="HomeScreen">
-      <MainStack.Screen name="HomeScreen" component={HomeScreen} />
+      <MainStack.Screen
+        options={() => ({
+          title: 'E-Market',
+          header: ({navigation, route, options}) => (
+            <CustomTopBar
+              options={options}
+              route={route}
+              navigation={navigation}
+            />
+          ),
+        })}
+        name="HomeScreen"
+        component={HomeScreen}
+      />
     </MainStack.Navigator>
   );
 };
